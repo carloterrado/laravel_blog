464c258ceceae5b77494a2a77bb902292c2a0c62
carloterrado <iamterradocarlo@gmail.com>
2023-04-09 21:32:57 +0800

    .

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,6 @@
+<?php return array (
+  'author-login-form' => 'App\\Http\\Livewire\\AuthorLoginForm',
+  'author-profile-details' => 'App\\Http\\Livewire\\AuthorProfileDetails',
+  'author-profile-header' => 'App\\Http\\Livewire\\AuthorProfileHeader',
+  'top-header' => 'App\\Http\\Livewire\\TopHeader',
+);
